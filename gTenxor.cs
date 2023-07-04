9c47aafc1f9b553380585e47e21fbf3da682ca1e
Jesus Altuve <jaltuve@gmail.com>
2023-07-04 12:23:30 -0400

    Bug Fixing

--- gTenxor.cs
+++ gTenxor.cs
@@ -135,7 +135,7 @@ namespace Bojote.gTenxor
                     {
                         if (SerialConnection.IsConnected)
                         {
-                            byte[] serialData = new byte[] { 0, 0 };
+                            byte[] serialData = new byte[] { 2, 3 };
                             SerialConnection.SerialPort.Write(serialData, 0, serialData.Length);
                         }
                     }
